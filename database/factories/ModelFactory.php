1cffe824415f1855356fab5ad418e2c1d40dbbff
MaryAnne45 <mary.jereh@gmail.com>
2018-10-19 12:16:27 +0300

    setting up yhr factories for test data

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+/*
+Factory allows you to  create test data
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+       'remember_token' => str_random(10),
+    ];
+});
