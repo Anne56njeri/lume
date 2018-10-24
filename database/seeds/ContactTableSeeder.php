75efc8ba301ccc67055771ba4278860b10eafe93
MaryAnne45 <mary.jereh@gmail.com>
2018-10-25 01:33:14 +0300

    added a seeder for the contact table

---
+++ database/seeds/ContactTableSeeder.php
@@ -0,0 +1,85 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Contact;
+
+class ContactTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+     /*
+     we loop through as we create the dummy data
+     */
+     public function run()
+       {
+         {
+         DB::table('contact')->delete();
+
+         Contact::create([
+             'name' => 'First account',
+             'email'=>'one@gmail.com'
+             'phone'=>'12345'
+             'address'=> 'Nairobi',
+             'company'=>'company1',
+             'title'=>'title1',
+             'notes'=>'note1',
+             'meetings'=>'meeeting1',
+             'opportunities'=>'opp1',
+             'preffered_notification_method'=>'one'
+             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+
+         ]);
+         Contact::create([
+             'name' => 'second account',
+             'email'=>'two@gmail.com'
+             'phone'=>'12345'
+             'address'=> 'kisumu',
+             'company'=>'company2',
+             'title'=>'title2',
+             'notes'=>'note2',
+             'meetings'=>'meeeting2',
+             'opportunities'=>'opp2',
+             'preffered_notification_method'=>'two'
+
+              'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+              'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+         ]);
+
+         Contact::create([
+             'name' => 'third account',
+             'email'=>'three@gmail.com'
+             'phone'=>'12345'
+             'address'=> 'Nakuru',
+             'company'=>'company3',
+             'title'=>'title3',
+             'notes'=>'note3',
+             'meetings'=>'meeeting3',
+             'opportunities'=>'opp3',
+             'preffered_notification_method'=>'two'
+             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+         ]);
+
+         Contact::create([
+             'name' => 'forth account',
+             'email'=>'four@gmail.com'
+             'phone'=>'12345'
+             'address'=> 'Eldoret',
+             'company'=>'company4',
+             'title'=>'title4',
+             'notes'=>'note4',
+             'meetings'=>'meeeting4',
+             'opportunities'=>'opp4',
+             'preffered_notification_method'=>'three'
+             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+             'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+         ]);
+        }
+   	}
+       }
